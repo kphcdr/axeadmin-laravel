f9a1cce6393fc2fce253525cc16054be93f269a2
kphcdr <kphcdr@163.com>
2019-09-02 20:01:30 +0800

    format

--- routes/axe.php
+++ routes/axe.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix"=>config("axe.url"),"middleware"=>"web",'namespace'=>'\\Axe\\Http\\Controllers'],function(){
-    Route:: get("login","LoginController@getLogin");
-    Route:: get("logout","LoginController@getLogout");
-    Route:: post("login","LoginController@postLogin");
+Route::group(["prefix" => config("axe.url"), "middleware" => "web", 'namespace' => '\\Axe\\Http\\Controllers'], function () {
+    Route::get("login", "LoginController@getLogin");
+    Route::get("logout", "LoginController@getLogout");
+    Route::post("login", "LoginController@postLogin");
 
-    Route::group(["middleware"=>"axe"],function(){
+    Route::group(["middleware" => "axe"], function () {
         Route::get("/", "IndexController@index");
-        Route::get("home","IndexController@home");
+        Route::get("home", "IndexController@home");
         //menu
         Route::resource("admin", "AdminController");
         Route::resource("menu", "MenuController");
