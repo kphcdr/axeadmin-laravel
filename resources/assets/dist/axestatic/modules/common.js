b98e35d9f72e65c994fd3255df51d32e2ad1a689
kphcdr <kphcdr@163.com>
2019-09-02 14:45:06 +0800

    composer.json

---
+++ resources/assets/dist/axestatic/modules/common.js
@@ -0,0 +1,2 @@
+/** layuiAdmin.std-v1.2.1 LPPL License By http://www.layui.com/admin/ */
+ ;layui.define(function(i){layui.$,layui.layer,layui.laytpl,layui.setter,layui.view,layui.admin;i("common",{})});
